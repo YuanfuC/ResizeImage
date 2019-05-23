4c1f62f1c0377734c5ac8c45d051f55a0047d651
ChenYuanfu <chenyuanfu@zerozero.cn>
2019-05-23 09:34:03 +0800

    Initial Commit

---
+++ ImageResize/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageResize
+//
+//  Created by ChenYuanfu on 2019/5/23.
+//  Copyright © 2019 ChenYuanfu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageResize/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageResize
+//
+//  Created by ChenYuanfu on 2019/5/23.
+//  Copyright © 2019 ChenYuanfu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
